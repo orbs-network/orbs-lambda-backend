c4f2ddc16f4e53f5844be1d973aac43b75c099b9
Idan Atar <idanatar@MacBook-Pro-6.local>
2022-09-04 08:54:42 +0300

    added while loop

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,16 @@ const myStatus = {
 let oldRevision = execSync('git rev-parse HEAD').toString().trim()
 
 async function main() {
-    console.log("Looking for Git changes...");
-    const newRevision = execSync('git rev-parse HEAD').toString().trim();
-    if (newRevision !== oldRevision) console.log("New commit found", newRevision);
-    oldRevision = newRevision;
+    while (true) {
+        console.log("Looking for Git changes...");
+        const newRevision = execSync('git rev-parse HEAD').toString().trim();
+        if (newRevision !== oldRevision) console.log("New commit found", newRevision);
+        oldRevision = newRevision;
 
-    myStatus.Timestamp = new Date().toISOString();
-    outputFileSync('./status/status.json', JSON.stringify(myStatus));
-    await new Promise(resolve => setTimeout(resolve, 60000));
+        myStatus.Timestamp = new Date().toISOString();
+        outputFileSync('./status/status.json', JSON.stringify(myStatus));
+        await new Promise(resolve => setTimeout(resolve, 60000));
+    }
 }
 
 main().then().catch(e=>console.log(e))
