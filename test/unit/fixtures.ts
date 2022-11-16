a583d8e56f2942958fb5adbfe3301b4ff988d384
idanatar <idana@orbs.com>
2022-11-16 15:20:25 +0200

    added unit tests

---
+++ test/unit/fixtures.ts
@@ -0,0 +1,117 @@
+export const guardians = {
+  "0x216FF847E6e1cf55618FAf443874450f734885e0": {
+    "weight": 274962420,
+    "nodeAddress": "0xf8cd99a88707c4fc64ac91f237af8570a83cc018",
+    "ip": "54.95.108.148"
+  },
+  "0x9520f53fd81c668e8088ae194c40e3f977b73d28": {
+    "weight": 274962420,
+    "nodeAddress": "0xf8cd99a88707c4fc64ac91f237af8570a83cc018",
+    "ip": "54.95.108.148"
+  },
+  "0xc5e624d6824e626a6f14457810e794e4603cfee2": {
+    "weight": 134813213,
+    "nodeAddress": "0x8cd2a24f0c3f50bce2f12c846277491433b47ae0",
+    "ip": "34.227.204.75"
+  },
+  "0x63aef7616882f488bca97361d1c24f05b4657ae5": {
+    "weight": 94449694,
+    "nodeAddress": "0xe00e13219734eec6e1e95b9a06bd881143d52bc9",
+    "ip": "13.112.58.64"
+  },
+  "0x8ddb908c77ccc9cfde28ddf84311cb6fdf3f3125": {
+    "weight": 68573652,
+    "nodeAddress": "0xcfb0a1637e9af76a544b3066a9ad24d935fcbe0e",
+    "ip": "52.78.29.172"
+  },
+  "0x0874bc1383958e2475df73dc68c4f09658e23777": {
+    "weight": 52784767,
+    "nodeAddress": "0x067a8afdc6d7bafa0ccaa5bb2da867f454a34dfa",
+    "ip": "46.101.165.46"
+  },
+  "0x0c56b39184e22249e35efcb9394872f0d025256b": {
+    "weight": 52784767,
+    "nodeAddress": "0x255c1f6c4da768dfd31f27057d38b84de41bcd4d",
+    "ip": "13.124.229.86"
+  },
+  "0x17fe98a222c41217c51c823352537dc542ad06ef": {
+    "weight": 52784767,
+    "nodeAddress": "0x21ff8c5d4fce6912272dc424241811daec5fdbf3",
+    "ip": "13.125.31.152"
+  },
+  "0x2b7b4ed553ee757f350e6ac637a49d50c5889a0d": {
+    "weight": 52784767,
+    "nodeAddress": "0x63e42a840cbdd950c138d4c20d70f049574dae24",
+    "ip": "65.108.141.152"
+  },
+  "0x3d726623456e34e8a7f5567f6249ec4d72cc3595": {
+    "weight": 52784767,
+    "nodeAddress": "0x28cc6746bf774ab7bab70f703fd857c86efc7835",
+    "ip": "3.34.238.235"
+  },
+  "0x5320a3141fe602909ad7a8bb811a38b44f1584d9": {
+    "weight": 52784767,
+    "nodeAddress": "0xda961133e213c0b3eeab6fb1a033c017846ae627",
+    "ip": "54.65.161.60"
+  },
+  "0x55fa13b0ed6502243ff69707ffbc2ef4c77d3555": {
+    "weight": 52784767,
+    "nodeAddress": "0x34ae49c15e66982b7789f84f5aa2ad7556ce20eb",
+    "ip": "34.224.206.180"
+  },
+  "0x69c57da3914693dbc5e96ff6c37216e70c6fbbc9": {
+    "weight": 52784767,
+    "nodeAddress": "0x6412cb7c5aea5e5e822a45c51e2a9c0e4027366f",
+    "ip": "3.142.204.182"
+  },
+  "0x6dda3d258e21abc37668e82ae543e8c77567f220": {
+    "weight": 52784767,
+    "nodeAddress": "0x79f088400728c380412b3c351ff65d1e06e40d68",
+    "ip": "15.164.83.34"
+  },
+  "0x7f0e5488a651c08f84df8e4088303c94c58a728b": {
+    "weight": 52784767,
+    "nodeAddress": "0x0838b80529ff7a785e1bd644c3bafc32f6d7811d",
+    "ip": "15.165.194.81"
+  },
+  "0x95a702a2896178b3123ed1a16e5c6a149b234fda": {
+    "weight": 52784767,
+    "nodeAddress": "0xb2d1b4db987d0f49e43a90df8614143c7a6f8b65",
+    "ip": "52.194.147.147"
+  },
+  "0x9b3853cc5734f18ab95e99e6fa4e7fb5da1e1186": {
+    "weight": 52784767,
+    "nodeAddress": "0xb226f469841d193829fa54661eac4ba5ce91a41e",
+    "ip": "3.36.152.137"
+  },
+  "0xaa72b70f008dec8db84d4338b8b281a3740f3898": {
+    "weight": 52784767,
+    "nodeAddress": "0xc16a9f64abf45d84aa337457247ca09e9cdaa7a0",
+    "ip": "3.37.152.23"
+  },
+  "0xc82ec0f3c834d337c51c9375a1c0a65ce7aadaec": {
+    "weight": 52784767,
+    "nodeAddress": "0x10736878dfc96d37ee24016d33e03a0ea88a3bd4",
+    "ip": "3.143.72.104"
+  },
+  "0xc90ef369d33e80e308e74896975baafd38cac0be": {
+    "weight": 52784767,
+    "nodeAddress": "0xa61a0e026b5699cd9d0c1dd37f2fc8cc813e4c7f",
+    "ip": "15.165.69.117"
+  },
+  "0xca565ccb6434e0adf49b2d66df12b0046b013d7f": {
+    "weight": 52784767,
+    "nodeAddress": "0x2333b7587558c48110ffbc821c2ea5eefed76d88",
+    "ip": "3.140.160.36"
+  },
+  "0xf0db4646e4be8e2094cd6d7c6d253a87eec7af58": {
+    "weight": 52784767,
+    "nodeAddress": "0x90e544fa6f0029e516ee433e736f1cbcdefc2630",
+    "ip": "15.165.22.8"
+  },
+  "0xf7ae622c77d0580f02bcb2f92380d61e3f6e466c": {
+    "weight": 52784767,
+    "nodeAddress": "0x5bcf21c33a7dfc6e5ed26f7439ef065075ea61cf",
+    "ip": "54.241.122.39"
+  }
+}
---
+++ test/unit/test.spec.ts
@@ -0,0 +1,66 @@
+import {expect} from "chai";
+import {beforeEach} from "mocha";
+import {Engine} from "../../src/engine";
+import {guardians} from "./fixtures";
+import {set} from 'mockdate'
+import {MS_TO_MINUTES, TASK_TIME_DIVISION_MIN} from "../../src/constants";
+import {intervalToMinutes, validateCron} from "../../dist/utils";
+
+describe("Engine", () => {
+    let instance: Engine;
+    const selfAddress = "0x216FF847E6e1cf55618FAf443874450f734885e0";
+    beforeEach(() => {
+        instance = new Engine({}, guardians, selfAddress, '')
+    })
+
+    describe("Leader election", () => {
+        describe("Time based leader", () => {
+            it("Should return true", () => {
+                set(TASK_TIME_DIVISION_MIN*MS_TO_MINUTES*Object.keys(guardians).length)
+                expect(instance.isLeaderTime()).to.be.true;
+            });
+        });
+        describe("Hash based leader", () => {
+            it("Should return true", () => {
+                expect(instance.isLeaderHash("0x09c3be8189eb5bdd9ab559dcddb3ae09d2b394a5e96fef3566a3232e088fa3")).to.be.true;
+            });
+        });
+    });
+
+    describe("Should run onInterval", () => {
+        it("Should always return true for 1m", () => {
+            expect(instance.shouldRunInterval("project", "1m")).to.be.true;
+        });
+        it("Should return true", () => {
+            set(1668410745)
+            expect(instance.shouldRunInterval("project", "2m")).to.be.true;
+        });
+        it("Should return false", () => {
+            set(1668410745)
+            expect(instance.shouldRunInterval("project1", "2m")).to.be.false;
+        });
+    });
+
+    describe("validateCron", () => {
+        it("Should return valid expression (len 5)", () => {
+            const pattern = "* * * * *";
+            expect(validateCron(pattern)).to.equal(pattern);
+        });
+        it("Should return valid expression (len 6)", () => {
+            const pattern = "* * * * * *";
+            expect(validateCron(pattern)).to.equal("* * * * *");
+        });
+        it("Should throw error", () => {
+            const pattern = "* * * *";
+            expect(() => validateCron(pattern)).to.throw();
+        });
+        it("Should throw invalid expression", () => {
+            const pattern = "* * * * x";
+            expect(() => validateCron(pattern)).to.throw();
+        });
+        it("Should throw invalid interval", () => {
+            const pattern = "1x";
+            expect(() => intervalToMinutes(pattern)).to.throw();
+        });
+    })
+})
