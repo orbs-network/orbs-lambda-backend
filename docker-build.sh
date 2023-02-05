9e7abc8253612e716725393a8c0ac71f65adc2b8
idanatar <idana@orbs.com>
2023-02-05 14:37:42 +0200

    debugging docker

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-ls /home/circleci/
-ls /home/circleci/.docker
-ls /home/circleci/.docker/buildx
-ls /home/circleci/.docker/buildx/instances
+mkdir -p /home/circleci/.docker/buildx/instances
 
 npm run build
 docker build -t orbsnetworkstaging/vm-lambda:$(cat .version) .
