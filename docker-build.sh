2b16dfe5430e3bcff0fd3ca860b5605065e4d358
idanatar <idana@orbs.com>
2023-02-05 14:50:40 +0200

    debugging docker

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir -p /home/circleci/.docker/buildx/instances
-touch /home/circleci/.docker/buildx/instances/default
-docker buildx create --use
+echo '{"Name":"reverent_diffie","Driver":"docker-container","Nodes":[{"Name":"reverent_diffie0","Endpoint":"unix:///var/run/docker.sock","Platforms":null,"Flags":null,"DriverOpts":{},"Files":null}],"Dynamic":false}' > /home/circleci/.docker/buildx/instances/default
 
 npm run build
 docker build -t orbsnetworkstaging/vm-lambda:$(cat .version) .
